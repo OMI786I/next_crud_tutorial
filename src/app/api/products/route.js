cad44202e3b36b7ccecc7e8003d03196856cfd94
OMI786I <sohrawardy1998@gmail.com>
2024-09-26 12:58:28 +0600

    create operation using mongoose and next yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeei

---
+++ src/app/api/products/route.js
@@ -0,0 +1,18 @@
+import connectMongoDB from "@/app/lib/mongodb";
+import ProductModel from "../../../../models/ProducntionModel";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  const { name, image, price, category } = await request.json();
+  await connectMongoDB();
+  const newProduct = await ProductModel.create({
+    name,
+    image,
+    price,
+    category,
+  });
+  return NextResponse.json(
+    { message: "Product Created", newProduct },
+    { status: 201 }
+  );
+}
--- src/app/create/page.jsx
+++ src/app/create/page.jsx
@@ -1,7 +1,10 @@
 "use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { useForm } from "react-hook-form";
+
 const page = () => {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
@@ -9,7 +12,26 @@ const page = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (data) => {
+    console.log(data);
+
+    try {
+      const res = await fetch("http://localhost:3000/api/products", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (res.ok) {
+        router.push("/");
+      } else {
+        throw new Error("Failed to create a new product");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
@@ -18,18 +40,71 @@ const page = () => {
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
             <label className="block text-sm font-medium text-gray-700 mb-2">
-              Required Field
+              Name Field
             </label>
             <input
-              {...register("exampleRequired", { required: true })}
+              {...register("name", { required: true })}
               className={`border w-full p-2 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none ${
-                errors.exampleRequired ? "border-red-500" : "border-gray-300"
+                errors.name ? "border-red-500" : "border-gray-300"
               }`}
-              placeholder="Enter something..."
+              placeholder="Enter Name..."
             />
-            {errors.exampleRequired && (
+            {errors.name && (
               <p className="text-red-500 text-sm mt-2">
-                This field is required
+                Name field is required
+              </p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700 mb-2">
+              Image Field
+            </label>
+            <input
+              {...register("image", { required: true })}
+              className={`border w-full p-2 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none ${
+                errors.image ? "border-red-500" : "border-gray-300"
+              }`}
+              placeholder="Enter image url..."
+            />
+            {errors.image && (
+              <p className="text-red-500 text-sm mt-2">
+                Image field is required
+              </p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700 mb-2">
+              Price Field
+            </label>
+            <input
+              {...register("price", { required: true })}
+              type="number"
+              className={`border w-full p-2 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none ${
+                errors.price ? "border-red-500" : "border-gray-300"
+              }`}
+              placeholder="Enter Price..."
+            />
+            {errors.price && (
+              <p className="text-red-500 text-sm mt-2">
+                Price field is required
+              </p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700 mb-2">
+              category Field
+            </label>
+            <input
+              {...register("category", { required: true })}
+              type="text"
+              className={`border w-full p-2 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none ${
+                errors.category ? "border-red-500" : "border-gray-300"
+              }`}
+              placeholder="Enter Price..."
+            />
+            {errors.category && (
+              <p className="text-red-500 text-sm mt-2">
+                category field is required
               </p>
             )}
           </div>
